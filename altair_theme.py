938a901faf92befde8f6b9d1784f889fbeeedc76
root <tuulos@gmail.com>
2024-08-11 03:43:13 +0000

    improvements

---
+++ altair_theme.py
@@ -0,0 +1,99 @@
+THEME={
+    "arc": {
+      "fill": "#30a2da"
+    },
+    "area": {
+      "fill": "#30a2da"
+    },
+    "axis": {
+      "domainColor": "#cbcbcb",
+      "grid": True,
+      "gridColor": "#cbcbcb",
+      "gridWidth": 1,
+      "labelColor": "#999",
+      "labelFontSize": 24,
+      "labelPadding": 4,
+      "tickColor": "#cbcbcb",
+      "tickSize": 10,
+      "titleColor": "#333",
+      "titleFontSize": 24,
+      "titlePadding": 10
+    },
+    "axisBand": {
+      "grid": False
+    },
+    "background": "#faf8f4",
+    "bar": {
+      "binSpacing": 2,
+      "fill": "#30a2da",
+      "stroke": None
+    },
+    "group": {
+      "fill": "#f0f0f0"
+    },
+    "legend": {
+      "labelColor": "#333",
+      "labelFontSize": 14,
+      "padding": 1,
+      "symbolSize": 30,
+      "symbolType": "square",
+      "titleColor": "#333",
+      "titleFontSize": 16,
+      "titlePadding": 20
+    },
+    "line": {
+      "stroke": "#30a2da",
+      "strokeWidth": 2
+    },
+    "path": {
+      "stroke": "#30a2da",
+      "strokeWidth": 0.5
+    },
+    "point": {
+      "filled": True,
+      "shape": "circle"
+    },
+    "range": {
+      "category": [
+        "#30a2da",
+        "#fc4f30",
+        "#e5ae38",
+        "#6d904f",
+        "#8b8b8b",
+        "#b96db8",
+        "#ff9e27",
+        "#56cc60",
+        "#52d2ca",
+        "#52689e",
+        "#545454",
+        "#9fe4f8"
+      ],
+      "diverging": [
+        "#cc0020",
+        "#e77866",
+        "#f6e7e1",
+        "#d6e8ed",
+        "#91bfd9",
+        "#1d78b5"
+      ],
+      "heatmap": [
+        "#d6e8ed",
+        "#cee0e5",
+        "#91bfd9",
+        "#549cc6",
+        "#1d78b5"
+      ]
+    },
+    "rect": {
+      "fill": "#30a2da"
+    },
+    "shape": {
+      "stroke": "#30a2da"
+    },
+    "title": {
+      "anchor": "start",
+      "fontSize": 24,
+      "fontWeight": 400,
+      "offset": 20
+    }
+  }
